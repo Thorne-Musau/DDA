fc90f4f679c1858490ad2e2be9e0a1aa9f38d859
Thorne-Musau <thornemusau@gmail.com>
2023-10-16 22:05:46 +0300

    Updated djk

--- dijkstra_algo.py
+++ dijkstra_algo.py
@@ -33,14 +33,19 @@ def dijkstra(graph, start):
 
 # Define a graph as an adjacency dictionary
 graph = {
-    'A': {'B': 1, 'C': 4},
-    'B': {'A': 1, 'C': 2, 'D': 5},
-    'C': {'A': 4, 'B': 2, 'D': 1},
-    'D': {'B': 5, 'C': 1}
+    '0': {'1': 4, '7': 8},
+    '1': {'0': 4, '2': 8, '7':11},
+    '2': {'1': 8, '3': 7, '5': 4, '8': 2},
+    '3': {'2': 7, '4': 9, '5': 14},
+    '4': {'3': 9, '5': 10},
+    '5': {'2': 4, '3': 14, '4': 10, '6':2},
+    '6': {'5': 2, '7': 1, '8': 6},
+    '7': {'0': 8, '1': 11, '6': 1, '8': 7},
+    '8': {'2': 2, '6': 6, '7': 7}
 }
 
 # Specify the start node
-start_node = input(print("Enter the start node: "))
+start_node = input("Enter the start node: ")
 
 # Run Dijkstra's algorithm
 distances, predecessors = dijkstra(graph, start_node)
