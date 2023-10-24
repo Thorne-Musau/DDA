e2bcd10e28c799c67c648b9dfba990d12c92b25d
Thorne-Musau <thornemusau@gmail.com>
2023-10-24 13:42:46 +0300

    Tree traversals

---
+++ Tree Traversals/inorder_traversal.py
@@ -0,0 +1,43 @@
+# Python3 program to for tree traversals
+# Left --> Root --> Right
+
+# A class that represents an individual node in a
+# Binary Tree
+class Node:
+	def __init__(self, key):
+		self.left = None
+		self.right = None
+		self.val = key
+
+
+# A function to do inorder tree traversal
+def printInorder(root):
+
+	if root:
+
+		# First recur on left child
+		printInorder(root.left)
+
+		# Then print the data of node
+		print(root.val, end=" "),
+
+		# Now recur on right child
+		printInorder(root.right)
+
+
+# Driver code
+# Changed values of the alphabets to their corresponding numerical values
+if __name__ == "__main__":
+	root = Node(1)
+	root.left = Node(2)
+	root.right = Node(3)
+	root.left.right = Node(4)
+	root.right.left = Node(5)
+	root.right.right = Node(6)
+	root.right.left.left = Node(7)
+	root.right.right.left = Node(8)
+	root.right.right.right = Node(9)
+
+	# Function call
+	print("Inorder traversal of binary tree is")
+	printInorder(root)
---
+++ Tree Traversals/preorder_traversal.py
@@ -0,0 +1,42 @@
+# Python3 program to for tree traversals
+# Root --> Left --> Right
+
+# A class that represents an individual node
+# in a Binary Tree
+class Node:
+	def __init__(self, key):
+		self.left = None
+		self.right = None
+		self.val = key
+
+
+# A function to do postorder tree traversal
+def printPostorder(root):
+
+	if root:
+
+		# First recur on left child
+		printPostorder(root.left)
+
+		# The recur on right child
+		printPostorder(root.right)
+
+		# Now print the data of node
+		print(root.val, end=" "),
+
+
+# Driver code
+if __name__ == "__main__":
+	root = Node(1)
+	root.left = Node(2)
+	root.right = Node(3)
+	root.left.right = Node(4)
+	root.right.left = Node(5)
+	root.right.right = Node(6)
+	root.right.left.left = Node(7)
+	root.right.right.left = Node(8)
+	root.right.right.right = Node(9)
+
+	# Function call
+	print("Postorder traversal of binary tree is")
+	printPostorder(root)
